961eb24d16f35f84c2ca8b5032382b8323128cd4
wztan <wztan@hotmail.co.uk>
2017-02-01 01:23:24 +0000

    added mario.py

---
+++ pset6/mario.py
@@ -0,0 +1,17 @@
+import cs50
+
+h = -1
+while (h < 0 or h > 23):
+    print("Height: ", end="")
+    h = cs50.get_int()
+    
+for i in range(h):
+    line = ""
+    for j in range(0, h - i - 1):
+        line += " "
+    for j in range(0, i + 1):
+        line += "#"
+    line += "  "
+    for j in range(0, i + 1):
+        line += "#"
+    print(line)
