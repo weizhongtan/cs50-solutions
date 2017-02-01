ecca4ce599d7b13eabc90e607561e190aa281327
wztan <wztan@hotmail.co.uk>
2017-02-01 02:53:27 +0000

    slightly simplified mario

--- pset6/mario.py
+++ pset6/mario.py
@@ -2,16 +2,16 @@ import cs50
 
 h = -1
 while (h < 0 or h > 23):
-    print("Height: ", end="")
+    print("Height: ", end = "")
     h = cs50.get_int()
     
 for i in range(h):
     line = ""
-    for j in range(0, h - i - 1):
+    for j in range(h-i-1):
         line += " "
-    for j in range(0, i + 1):
+    for j in range(i+1):
         line += "#"
     line += "  "
-    for j in range(0, i + 1):
+    for j in range(i+1):
         line += "#"
     print(line)
